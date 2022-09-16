d76ce1c6220549bbfa43525a930a5fca6c91da94
AugustoOL <auoliveira43@gmail.com>
2022-09-16 02:52:37 -0300

    desafio

--- tests/order-sucess.spec.ts
+++ tests/order-sucess.spec.ts
@@ -2,12 +2,8 @@ import { test, expect } from "@playwright/test";
 
 test('compra com sucesso', async ({page}) => {
 
-    //vai paraa página de Login
     await page.goto("https://saucedemo.com/");
-    //const title = page.locator('.App-header p');
-    //await expect(title).toHaveText('Login');
 
-    //Submeter o formulario de login
     await page.fill('input[placeholder*=Username]', 'standard_user')
     await page.fill('input[placeholder*=Password]', 'secret_sauce')
     await page.click('input#login-button.submit-button.btn_action')
